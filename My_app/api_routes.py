6e13aca5681e7ba07e46986114f4884fb3f924e5
Joshi Hiren <hirenjoshi0456@gmail.com>
2026-09-24 12:28:12 +0530

    Add Flask Blueprint with root and home routes

--- My_app/api_routes.py
+++ My_app/api_routes.py
@@ -1,1 +1,19 @@
+from flask import request,Blueprint
 
+api_bp = Blueprint("api", __name__)
+
+@api_bp.route("/")
+def blueprint_root():
+    print(request.method)
+    if request.method=='GET':
+        return "Hello From Flask Blueprint"
+    else:
+        return "Invalid Request"
+
+@api_bp.route("/home")
+def blueprint_root_home():
+    print(request.method)
+    if request.method=='GET':
+        return "Hello From Flask Blueprint Home"
+    else:
+        return "Invalid Request"
